99689ddcd829900b47fc9e65ad96c95da4a07ca4
k-szczech <kszczech@hawk.iit.edu>
2022-11-06 17:18:08 -0600

    Added name and wrote JS code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
 //add your javascrip between these two lines of code
- 
+	document.getElementById("entrybutton").addEventListener("click", displayResult);
+}
 
-
-
-
-
+function displayResult() {
+	var text = document.getElementById("entryinput").value;
+	alert("Krystian Szczech: " + text);
+	document.getElementById("textoutput").innerText = text;
+}
 
 
 window.addEventListener('load', init);
